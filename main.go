b2c16b1081893bf07c14c3aa57ba2ae24bdae2eb
Azer Abdullaev <azer.abdullaev.berlin@gmail.com>
2023-05-23 22:26:35 +0200

    Loading mpdscribble config and authenticating

--- main.go
+++ main.go
@@ -6,7 +6,14 @@ import (
 	"time"
 
 	"github.com/fhs/gompd/v2/mpd"
+	"gopkg.in/ini.v1"
 )
+
+func logErr(description string, err error) {
+	if err != nil {
+		log.Println(description + ": " + err.Error())
+	}
+}
 
 func main() {
 	// Connect to MPD server
@@ -15,6 +22,18 @@ func main() {
 		log.Fatalln(err)
 	}
 	defer conn.Close()
+
+	cfg, err := ini.Load("/etc/mpdscribble.conf")
+	logErr("Failed to load mpdscribble config", err)
+
+	username := cfg.Section("last.fm").Key("username").String()
+	password := cfg.Section("last.fm").Key("password").String()
+	apikey := cfg.Section("api").Key("apikey").String()
+	apisecret := cfg.Section("api").Key("apisecret").String()
+
+	api := lastfm.New(apikey, apisecret)
+	err = api.Login(username, password)
+	logErr("Failed to login on LastFM", err)
 
 	line := ""
 	line1 := ""
