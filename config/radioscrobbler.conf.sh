ad98c15b00f5cfa29e54a0553708f2002c9661a4
Azer Abdullaev <azer.abdullaev.berlin@gmail.com>
2024-01-07 19:46:23 +0100

    HEREDOC fix

--- config/radioscrobbler.conf.sh
+++ config/radioscrobbler.conf.sh
@@ -9,7 +9,7 @@ PASSWORD=$(echo "${DATA}" | yq -r '.password')
 APIKEY=$(echo "${DATA}" | yq -r '.apikey')
 APISECRET=$(echo "${DATA}" | yq -r '.apisecret')
 
-cat <EOF > ${HOME}/.config/radioscrobbler/radioscrobbler.conf
+cat <<EOF > ${HOME}/.config/radioscrobbler/radioscrobbler.conf
 [source.mpd]
 host = 127.0.0.1
 port = 6600
