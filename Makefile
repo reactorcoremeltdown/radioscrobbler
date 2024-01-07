b0766b72e218021fa8b3538615c2c309005604e6
Azer Abdullaev <azer.abdullaev.berlin@gmail.com>
2024-01-07 19:42:19 +0100

    Real world deploy to RPi 4 at home

--- Makefile
+++ Makefile
@@ -3,5 +3,12 @@ all: build
 build:
 	go get && go build
 
-install:
+install: build
+	vault-request-unlock
+	bash config/radioscrobbler.conf.sh
+	vault-request-lock
 	install -m 755 ./radioscrobbler /usr/local/bin
+	install -D -m 644 config/systemd/radioscrobbler.service /etc/systemd/system
+	systemctl daemon-reload
+	systemctl enable radioscrobbler.service
+	systemctl restart radioscrobbler.service
---
+++ config/radioscrobbler.conf.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+#
+
+test -d ${HOME}/.config/radioscrobbler || mkdir -p ${HOME}/.config/radioscrobbler
+
+DATA=$(vault-request-key credentials lastfm)
+USERNAME=$(echo "${DATA}" | yq -r '.username')
+PASSWORD=$(echo "${DATA}" | yq -r '.password')
+APIKEY=$(echo "${DATA}" | yq -r '.apikey')
+APISECRET=$(echo "${DATA}" | yq -r '.apisecret')
+
+cat <EOF > ${HOME}/.config/radioscrobbler/radioscrobbler.conf
+[source.mpd]
+host = 127.0.0.1
+port = 6600
+
+[source.macos]
+exec_path = /usr/local/bin//nowplaying-cli
+interval = 5
+
+[destination.lastfm]
+username = ${USERNAME}
+password = ${PASSWORD}
+apikey = ${APIKEY}
+apisecret = ${APISECRET}
+EOF
